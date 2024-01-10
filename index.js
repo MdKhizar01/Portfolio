399a76915174add609b898ef0f12566152c602f3
Md Momin Khizar Uddin <90639281+MdKhizar01@users.noreply.github.com>
2024-01-10 22:49:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function redirectTo(page) {
+    // Set the href to the specified local URL
+    window.location.href = page;
+}
+function redirectToGmail() {
+    // Set the href to the Gmail URL
+    window.location.href = "https://mail.google.com/mail/u/0/#inbox";
+}
+
